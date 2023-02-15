4d75c446f0ee2076e254ed9598db3cbc7cacda16
Ann <ann.mushka@gmail.com>
2023-02-15 13:06:20 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    let total = Object.values(this.state).reduce((sum, elem) => {
+      return sum + elem;
+    }, 0);
+    return total;
+  }
+
+  countPositiveFeedbackPercentage() {
+    let total = this.countTotalFeedback();
+    let positive = Math.round((this.state.good / total) * 100);
+    return positive;
+  }
+
+  handleClick = name => {
+    this.setState(prevState => {
+      return { [name]: prevState[name] + 1 };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    let total = this.countTotalFeedback();
+
+    return (
+      <main>
+        <Section title="Please Leave Feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            handleClick={this.handleClick}
+          />
+        </Section>
+
+        {total ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage() || 0}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </main>
+    );
+  }
+}
